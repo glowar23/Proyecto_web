4ca706b08d7a5d0f4c5ae88ed69bbb36030620f2
edgarml01 <egarmola08@gmail.com>
2024-04-06 18:46:59 -0600

    add foter_tienda

---
+++ Views/Generals/footer_tienda.php
@@ -0,0 +1,51 @@
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script src=<?php media()."js/"."carrito.js"?>></script>
+    <div class="container">
+  <footer class="row row-cols-1 row-cols-sm-2 row-cols-md-5 py-5 my-5 border-top">
+    <div class="col mb-3">
+      <a href="<?=base_url()?>" class="d-flex align-items-center mb-3 link-body-emphasis text-decoration-none">
+      <p class="title">MyPets</p>
+      </a>
+      <p class="text-body-secondary">© 2024</p>
+    </div>
+
+    <div class="col mb-3">
+
+    </div>
+
+    <div class="col mb-3">
+      <h5 class="title">Sobre MyPets</h5>
+      <ul class="nav flex-column">
+        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Home</a></li>
+        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Features</a></li>
+        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Pricing</a></li>
+        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">FAQs</a></li>
+        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">About</a></li>
+      </ul>
+    </div>
+
+    <div class="col mb-3">
+      <h5> Contacto</h5>
+      <ul class="nav flex-column">
+        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Home</a></li>
+        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Features</a></li>
+        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Pricing</a></li>
+        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">FAQs</a></li>
+        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">About</a></li>
+      </ul>
+    </div>
+
+    <div class="col mb-3">
+      <h5>Informacion Legal</h5>
+      <ul class="nav flex-column">
+        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Home</a></li>
+        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Features</a></li>
+        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Pricing</a></li>
+        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">FAQs</a></li>
+        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">About</a></li>
+      </ul>
+    </div>
+  </footer>
+</div>
+</body>
+</html>
--- Views/home.php
+++ Views/home.php
@@ -3,39 +3,6 @@
     require_once('Views/Generals/header_tienda.php');
     
 ?>
-    <div class="container-items">    
-            <?php
-            if (count($arrProductos)==0){
-                echo '<h1>No hay productos</h1>';
-            }else {
-                $arrPr=[];
-                for ($i=0; $i <count($arrProductos) ; $i++) {
-                    if (count($arrProductos[$i]['images'])>0){
-                        $portada = $arrProductos[$i]['images'][0]['url_image'];
-                    }
-                    else{
-                        $portada=media().'images/default-image.jpg';
-                    } ?> 
-                        <div class="item">
-                            <figure>
-                                <img src="<?=$portada?>" alt="producto"> 
-                            </figure>
-                            <div>
-                                <div class="info-product">
-                                    <h2>
-                                        <?=$arrProductos[$i]['nombre_producto']?>
-                                        <p class="price">
-                                        <?=$arrProductos[$i]['precio']?>
-                                        </p>
-                                        <button> Añadir al carrito</button>
-                                    </h2>
-                                </div>
-                            </div>
-                        </div>
-                    <?php } ?>
-            <?php } ?>
-    
-    </div>
     <div class="container">
         <div id="carouselExample" class="carousel slide">
             <div class="carousel-inner">
@@ -81,9 +48,40 @@
             </button>
         </div>
     </div>
+    <div class="container-items">    
+            <?php
+            if (count($arrProductos)==0){
+                echo '<h1>No hay productos</h1>';
+            }else {
+                $arrPr=[];
+                for ($i=0; $i <count($arrProductos) ; $i++) {
+                    if (count($arrProductos[$i]['images'])>0){
+                        $portada = $arrProductos[$i]['images'][0]['url_image'];
+                    }
+                    else{
+                        $portada=media().'images/default-image.jpg';
+                    } ?> 
+                        <div class="item">
+                            <figure>
+                                <img src="<?=$portada?>" alt="producto"> 
+                            </figure>
+                            <div>
+                                <div class="info-product">
+                                    <h2>
+                                        <?=$arrProductos[$i]['nombre_producto']?>
+                                        <p class="price">
+                                        <?=$arrProductos[$i]['precio']?>
+                                        </p>
+                                        <button> Añadir al carrito</button>
+                                    </h2>
+                                </div>
+                            </div>
+                        </div>
+                    <?php } ?>
+            <?php } ?>
     
+    </div>
     
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-    <script src=<?php media()."js/"."carrito.js"?>></script>
-</body>
-</html>
+<?php require_once('Views/Generals/footer_tienda.php');?>
+    
+ 
