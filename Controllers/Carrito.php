d056624ff11b7772923996ceb07c166995fbcbd4
edgarml01 <egarmola08@gmail.com>
2024-04-17 10:16:41 -0600

    Carrito

--- Controllers/Carrito.php
+++ Controllers/Carrito.php
@@ -7,7 +7,7 @@
 		public function __construct()
 		{
 			parent::__construct();
-			
+			session_start();
 		}
 
 		public function carrito()
--- Views/Carrito/carrito.php
+++ Views/Carrito/carrito.php
@@ -2,5 +2,5 @@
     require_once('Views/Generals/header_tienda.php');
 ?>
 	<h2>Este es la pagina de carrito.</h2>
-
+    <?= $_SESSION['arrIdProductos'][0]?>
 	<?php require_once('Views/Generals/footer_tienda.php');?>
