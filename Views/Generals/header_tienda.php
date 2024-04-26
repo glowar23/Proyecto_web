974299ee2e14fb0d784e07b956e4005083189fe9
edgarml01 <egarmola08@gmail.com>
2024-04-25 20:23:12 -0600

    Vista categoria

--- Views/Generals/header_tienda.php
+++ Views/Generals/header_tienda.php
@@ -5,14 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $data['page_title']?></title>
     
-    <script src="https://kit.fontawesome.com/3c4b3c6940.js" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/fccbf69fc7.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css" />
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="<?= media(); ?>css/styleProduct.css">
-    <link rel="stylesheet" type="text/css" href="<?=media(); ?> css/vendor.css">
+    <link rel="stylesheet" type="text/css" href="<?=media(); ?>css/vendor.css">
     <link rel="stylesheet" type="text/css" href="<?=media(); ?>css/style2.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
--- Views/Tienda/categoria.php
+++ Views/Tienda/categoria.php
@@ -1,8 +1,69 @@
-<?php 
+<?php     
     
     require_once('Views/Generals/header_tienda.php');
+    $arrProductos=$data['productos'];
+    $categorias=$data['categorias'];
 ?>
+    <div class="row">
+        <div class="col-md-3">
+            
+        </div>
+        <div class="col-md-9">
+            <h2 class="p-3 title"><?=($data['infoCategoria']['categoria'])?></h2>    
+        </div>
+        
+    </div>
+    <div class="row">
+        <div class="col-md-2">
+            <h4 class="p-3 title">Categorias</h4>
+        <div class="container m-4">
+            
+            <ul class="list-group">
+                <?php 
+                    foreach ($categorias as $cat) {
+                        $url=base_url().'/tienda/categoria/'.$cat['idcategoria'];
+                        echo '<a href="'.$url.'"><li class="list-group-item btn btn-secondary text-start">'.$cat['nombre'].'</li></a>';
+                    }
+                ?>
+        </ul>
+            </div>
+        </div>
+        <br>
+    <div class="container-items container col-md-10 mr-2">    
+            <?php
+            if (count($arrProductos)==0){
+                echo '<h1>No hay productos</h1>';
+            }else {
+                $arrPr=[];
+                for ($i=0; $i <count($arrProductos) ; $i++) {
+                    if (count($arrProductos[$i]['images'])>0){
+                        $portada = $arrProductos[$i]['images'][0]['url_image'];
+                    }
+                    else{
+                        $portada=media().'images/default-image.jpg';
+                    } ?> 
+                        <div class="item">
+                            <figure>
+                                <img src="<?=$portada?>" alt="producto"> 
+                            </figure>
+                            <div>
+                                <div class="info-product">
+                                    <h2>
+                                        <?=$arrProductos[$i]['nombre_producto']?>
+                                        <p class="price">
+                                        <?='$'.$arrProductos[$i]['precio']?>
+                                        </p>
+                                        <button class="boton btn" data-product="<?=$arrProductos[$i]['idproductos']?>"> Añadir al carrito</button>
+                                    </h2>
+                                </div>
+                            </div>
+                        </div>
+                    <?php } ?>
+            <?php } ?>
+    
+    </div>
+    </div>
+    
 
-
-<?php echo json_encode($data['productos']);
+<?php 
 require_once('Views/Generals/footer_tienda.php');?>
